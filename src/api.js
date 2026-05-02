661a5d91f2a2a5a13744ab0cd806f0b70db00d85
Dileep Kumar <mdileepkumar578@gmail.com>
2026-05-02 17:33:39 +0530

    update api url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://team-task-backend-production-6748.up.railway.app/"
 });
 
 // Attach token automatically
